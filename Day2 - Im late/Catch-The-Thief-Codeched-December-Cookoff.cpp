1fed7818f94b8d460c62083ee10df21da1977a11
vhaegar2615 <53910913+vhaegar1526@users.noreply.github.com>
2020-12-21 23:45:17 +0530

    Optimised soln2.

--- Day2 - Im late/Catch-The-Thief-Codeched-December-Cookoff.cpp
+++ Day2 - Im late/Catch-The-Thief-Codeched-December-Cookoff.cpp
@@ -13,13 +13,35 @@ string can_thief_be_caught(ll x, ll y, ll k, ll n){
     
 }
 
+string can_thief_be_caught1(ll x, ll y, ll k, ll n){
+    if(x==y)
+        return "YES\n";
+    int a, b;
+    if(x>y){
+        a= x;
+        b= y;
+    }
+    else{
+        a= y;
+        b= x;
+    }
+    
+    while(a>=b){
+        if(a==b)
+            return "YES\n";
+        a-=k;
+        b+=k;
+    }
+    return "NO\n";
+}
+
 int main(void){
 
     int t; cin >> t;
     while(t--){
         ll x, y, k, n; 
         cin >> x >> y >> k >> n;
-        cout << can_thief_be_caught(x, y, k, n);
+        cout << can_thief_be_caught1(x, y, k, n);
     }
     return 0;
 }
