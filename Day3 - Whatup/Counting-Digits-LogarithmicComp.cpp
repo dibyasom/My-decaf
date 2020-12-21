26313e9408373012564130b0c361b73eabb1b19f
vhaegar2615 <53910913+vhaegar1526@users.noreply.github.com>
2020-12-22 01:12:41 +0530

    Day3 begins!

---
+++ Day3 - Whatup/Counting-Digits-LogarithmicComp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+typedef unsigned long long int ll;
+
+
+int main(void){
+
+    ll num; cin >> num;
+    cout << num_of_digits(num) << endl;
+    return 0;
+}
