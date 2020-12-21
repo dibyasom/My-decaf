c4cbb2338a246c019c161dc43970476c30d63d8e
vhaegar2615 <53910913+vhaegar1526@users.noreply.github.com>
2020-12-22 01:16:50 +0530

    Counting digits in O(logn)

--- Day3 - Whatup/Counting-Digits-LogarithmicComp.cpp
+++ Day3 - Whatup/Counting-Digits-LogarithmicComp.cpp
@@ -1,9 +1,12 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 typedef unsigned long long int ll;
 
+ll num_of_digits(ll num){ //Returns number of digits <Only for natural numbers>
+    return floor(log10(num)+1);
+}
 
 int main(void){
 
