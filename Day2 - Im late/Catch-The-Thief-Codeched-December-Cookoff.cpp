909b54f08c8593ff393bf2ebf63c99a7be2e8985
vhaegar2615 <53910913+vhaegar1526@users.noreply.github.com>
2020-12-22 13:03:23 +0530

    December-cookoff 2020, solved 2/5th.

--- Day2 - Im late/Catch-The-Thief-Codeched-December-Cookoff.cpp
+++ Day2 - Im late/Catch-The-Thief-Codeched-December-Cookoff.cpp
@@ -5,34 +5,9 @@ using namespace std;
 typedef long long int ll;
 
 string can_thief_be_caught(ll x, ll y, ll k, ll n){
-    if(x==y)
-        return "YES\n";
-    else if(!(abs(x-y)%(2*k)))
-        return "YES\n";
-    return "NO\n";
-    
-}
-
-string can_thief_be_caught1(ll x, ll y, ll k, ll n){
-    if(x==y)
-        return "YES\n";
-    int a, b;
-    if(x>y){
-        a= x;
-        b= y;
-    }
-    else{
-        a= y;
-        b= x;
-    }
-    
-    while(a>=b){
-        if(a==b)
-            return "YES\n";
-        a-=k;
-        b+=k;
-    }
-    return "NO\n";
+    if(!(abs(x-y) %(2*k)))
+        return "YES";
+    return "NO"; 
 }
 
 int main(void){
@@ -41,7 +16,7 @@ int main(void){
     while(t--){
         ll x, y, k, n; 
         cin >> x >> y >> k >> n;
-        cout << can_thief_be_caught1(x, y, k, n);
+        cout << can_thief_be_caught(x, y, k, n) << endl;
     }
     return 0;
 }
