1fc3070941a515588eca53fce52a6f813cc575a9
vhaegar2615 <53910913+vhaegar1526@users.noreply.github.com>
2020-12-23 23:48:47 +0530

    Solved self-destructing-strings, codechef December-cookoff(2020)

--- Day4 - Almost Missed/self-destructing-strings.cpp
+++ Day4 - Almost Missed/self-destructing-strings.cpp
@@ -2,12 +2,44 @@
 
 using namespace std;
 
+int collapse_string(string str){
+    int size = str.length(), l = 0, r = 1;
+    
+    if(size%2)
+        return -1;
+
+    for(int i=0; i<size; i++){
+        if(l<size && r<size){
+            str[l]='*'; str[r]='*';
+        }
+        if((l<size && r<size) && (str[l] != str[r])){
+            if(l-1 < 0){
+                l = i+1;
+                r = l+1;
+                i++;
+            }
+            else{
+                l = i-1;
+                r = i+2;
+                i++;
+            }
+        }
+        else{
+            l=r;
+            r++;
+        }
+    }
+
+    cout << str << endl;
+    return 0;
+}
+
 int main(void){
 
     int t; cin >> t;
     while(t--){
         string str; cin >> str;
-        
+        cout << collapse_string(str) << endl;
     }
     return 0;
 }
