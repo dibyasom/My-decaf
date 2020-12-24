c0920f8ad0e2c79238862302ddfcf589355745cb
vhaegar2615 <53910913+vhaegar1526@users.noreply.github.com>
2020-12-24 16:46:38 +0530

    Optimised self-destructing-strings.

--- Day4 - Almost Missed/self-destructing-strings.cpp
+++ Day4 - Almost Missed/self-destructing-strings.cpp
@@ -3,37 +3,18 @@
 using namespace std;
 
 int collapse_string(string str){
-    int size = str.length(), l = 0, r = 1, rem = 0;
     
-    if(size%2)
+    if(str.length()%2)
         return -1;
+    long zeros= 0, ones= 0;
+    for(auto dig: str)
+        if(dig== '0')
+            zeros++;
+        else
+            ones++;
 
-    for(int i=0; i<size; i++){
-        if((l<size && r<size) && (str[l] != str[r])){
-             str[l]='*'; str[r]='*';
-            if(l-1 < 0){
-                l = i+1;
-                r = l+1;
-                i++;
-            }
-            else{
-                l = i-1;
-                r = i+2;
-                i++;
-            }
-        }
-        else{
-            l=r;
-            r++;
-        }
-        cout << str;
-        printf("\ti=%d\tl=%d\tr=%d\n\n", i, l, r);
-    }
-
-    for(int i=0; i<size; i++)
-        if(str[i]!='*')
-            rem++;
-    return rem/2;
+    return abs(zeros - ones)/2;
+    
 }
 
 int main(void){
