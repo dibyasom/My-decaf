e36749f18dced3e46cff4e22f5a2030859cda70e
vhaegar1526 <dibya.secret@gmail.com>
2020-12-30 22:52:14 +0530

    Day6, I'm kinda sick.

---
+++ Day7 - Imma sicko/icpcr.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long reciept(string n){
+    int max= -1, ind; long len= n.length(), dig= 0;
+    while(dig< len){
+        if(n[dig]>)
+    }
+}
+
+int main(void){
+
+    int t; cin >> t;
+    while(t--){
+        string n; cin >> n;
+        reciept(n);
+    }
+    return 0;
+}
