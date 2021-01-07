38eadb7e7c34ec3250e5efc53de929fa68dec9bf
vhaegar1526 <dibya.secret@gmail.com>
2021-01-08 01:56:30 +0530

    Computing powers in log(n) time comp. and O(1) space comp.

--- Day13 - Feels exciting./compute-power-inplace.cpp
+++ Day13 - Feels exciting./compute-power-inplace.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+typedef long long int ll;
+
+ll power_inplace(ll n, ll p){
+    ll raised= n, power= 1;
+    while(p>0){
+        if(p&1)
+            power*= raised;
+        raised*= raised;
+        p= p>>1;
+    }
+    return power;
+}
+
 int main(void){
 
-    
+    ll n, p; cin >> n >> p;
+    cout << power_inplace(n, p) << endl;
     return 0;
 }
