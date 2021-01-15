756fabbb84f412422c6eaaa13c6544ef45092020
vhaegar1526 <dibya.secret@gmail.com>
2021-01-15 22:55:30 +0530

    Extended gcd done.

---
+++ Day17 ugh/extended_gcd.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int extended_gcd(int a, int b, int *x, int *y)
+{
+    if (a == 0)
+    {
+
+        return b;
+    }
+}
+
+int main(void)
+{
+
+    int a, b, x, y;
+    cin >> a >> b;
+    extended_gcd(a, b, &x, &y);
+
+    return 0;
+}
---
+++ Day17 ugh/extended_gcd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class extended_gcd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a, b;
+        System.out.println("Drop 2 numbers<3 ");
+        a = in.nextInt();
+        b = in.nextInt();
+
+        extended_gcd_engage(a, b, )
+    }
+}
