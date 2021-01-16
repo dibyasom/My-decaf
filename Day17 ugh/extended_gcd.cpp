4d395a282cf869744b006a383a8575550324acfb
vhaegar1526 <dibya.secret@gmail.com>
2021-01-16 23:39:10 +0530

    Day18/ Bitwise operations./

--- Day17 ugh/extended_gcd.cpp
+++ Day17 ugh/extended_gcd.cpp
@@ -6,9 +6,17 @@ int extended_gcd(int a, int b, int *x, int *y)
 {
     if (a == 0)
     {
-
+        *x = 0;
+        *y = 1;
         return b;
     }
+
+    int x1, y1;
+    int gcd = extended_gcd(b % a, a, &x1, &y1);
+
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+    return gcd;
 }
 
 int main(void)
@@ -16,7 +24,8 @@ int main(void)
 
     int a, b, x, y;
     cin >> a >> b;
-    extended_gcd(a, b, &x, &y);
+    cout << "gcd -> " << extended_gcd(a, b, &x, &y) << endl;
+    cout << "coeffs -> " << x << ", " << y << endl;
 
     return 0;
 }
