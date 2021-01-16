bc0206acb8a1a6f65441e73cc883fb794c299522
vhaegar1526 <dibya.secret@gmail.com>
2021-01-16 23:49:44 +0530

    And, Or, Xor

--- Day18 Bit logix/and-or-xor.cpp
+++ Day18 Bit logix/and-or-xor.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+#define and_(x, y) (x & y)
+#define or_(x, y) (x | y)
+#define xor_(x, y) (x ^ y)
+
 int main(void)
 {
-
+    int x = 5, y = 2;
+    //Bitwise AND
+    cout << and_(x, y) << endl;
+    //Bitwise OR
+    cout << or_(x, y) << endl;
+    //Bitwise XOR
+    cout << xor_(x, y) << endl;
     return 0;
 }
