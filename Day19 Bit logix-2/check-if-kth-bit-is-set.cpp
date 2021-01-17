a98795489b8c8377b3f0ae23af1b4c38b255960c
vhaegar1526 <dibya.secret@gmail.com>
2021-01-17 23:18:27 +0530

    Check kth bit(from the right) is set or not.

---
+++ Day19 Bit logix-2/check-if-kth-bit-is-set.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+
+bool check_kth_bit(ll, ll);
+
+ll power_itr(ll n, ll p)
+{
+    ll pow = 1, power = n;
+    while (p)
+    {
+        if (p & 1)
+            pow *= power;
+        power *= power;
+        p >>= 1;
+    }
+
+    return pow;
+}
+
+bool check_kth_bit(ll num, ll k)
+{
+    ll twoPowerK = power_itr(2, k);
+    return (twoPowerK & num) == twoPowerK;
+}
+
+int main(void)
+{
+    ll num, k;
+    cin >> num >> k;
+    cout << check_kth_bit(num, k - 1) << endl;
+    return 0;
+}
