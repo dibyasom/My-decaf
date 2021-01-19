322adb2f417d2a2ebdb44580b25a046cac8b3aa7
vhaegar1526 <dibya.secret@gmail.com>
2021-01-20 00:16:37 +0530

    lol

--- Day21 stuff/2odd-occuring.cpp
+++ Day21 stuff/2odd-occuring.cpp
@@ -6,9 +6,21 @@ typedef long long int ll;
 
 void filter_odd_occuring(vector<ll> &arr)
 {
-    int res = 0, len = arr.size();
+    int res = 0, len = arr.size(), k = 0, setA = 0, setB = 0;
     for (ll i = 0; i < len; i++)
         res ^= arr[i];
+    while ((res & 1) == 0)
+    {
+        res >>= 1;
+        k++;
+    }
+    for (ll i = 0; i < len; i++)
+        if ((arr[i] >> k) & 1)
+            setA ^= arr[i];
+        else
+            setB ^= arr[i];
+
+    cout << setA << " " << setB << endl;
 }
 
 int main(void)
