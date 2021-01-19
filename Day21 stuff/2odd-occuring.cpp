0be30c97a289ec33cc198766b25947d2eec57df9
vhaegar1526 <dibya.secret@gmail.com>
2021-01-20 00:04:00 +0530

    lol

--- Day21 stuff/2odd-occuring.cpp
+++ Day21 stuff/2odd-occuring.cpp
@@ -1,1 +1,20 @@
-#
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+
+void filter_odd_occuring(vector<ll> &arr)
+{
+    int res = 0, len = arr.size();
+    for (ll i = 0; i < len; i++)
+        res ^= arr[i];
+}
+
+int main(void)
+{
+    vector<ll> arr = {1, 2, 1, 1, 3, 1, 5, 5};
+    filter_odd_occuring(arr);
+
+    return 0;
+}
