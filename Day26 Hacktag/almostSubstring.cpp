294d873702aaa9e9d788040e130b3deee1e45600
vhaegar1526 <dibya.secret@gmail.com>
2021-02-27 20:08:45 +0530

    Uber hacktag CP code.

---
+++ Day26 Hacktag/almostSubstring.cpp
@@ -0,0 +1,10 @@
+int almostSubstr(string t, string s){
+    for(int i=0; i<t.length(); i++){
+        if(s[0]==t[i]){
+            int sChar = 0;
+            while(sChar<s.length && (i+sChar)<t.length() && s[sChas]==t[i+sChar]){
+
+            }
+        }
+    }
+}
